a75fb7eb47f9692c9c4527ec473dc3144a125e8c
sehwanpark <skyman12345@naver.com>
2024-11-19 02:56:38 +0900

    Initial Commit

---
+++ BabsangLabTests/BabsangLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  BabsangLabTests.swift
+//  BabsangLabTests
+//
+//  Created by 박세환 on 11/19/24.
+//
+
+import Testing
+@testable import BabsangLab
+
+struct BabsangLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
