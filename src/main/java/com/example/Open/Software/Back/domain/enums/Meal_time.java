d7a6f6fdb474966438916d3c456bf924906ccfc8
HyundongdongYun <my4719cuswo12@gmail.com>
2024-11-22 22:31:55 +0900

    Meal time, Weight_goal eNums 삭제

--- src/main/java/com/example/Open/Software/Back/domain/enums/Meal_time.java
+++ src/main/java/com/example/Open/Software/Back/domain/enums/Meal_time.java
@@ -1,4 +1,4 @@
-package com.example.Open.Software.Back.enums;
+package com.example.Open.Software.Back.domain.enums;
 
 public enum Meal_time {
     breakfast, lunch, dinner, snack
--- src/main/java/com/example/Open/Software/Back/domain/enums/Weight_goal.java
+++ src/main/java/com/example/Open/Software/Back/domain/enums/Weight_goal.java
@@ -1,4 +1,4 @@
-package com.example.Open.Software.Back.enums;
+package com.example.Open.Software.Back.domain.enums;
 
 public enum Weight_goal {
     lose, maintain, gain
