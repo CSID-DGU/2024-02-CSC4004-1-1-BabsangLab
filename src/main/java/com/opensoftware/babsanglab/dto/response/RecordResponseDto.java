9fc886f08dadaed691062356f86fd8c6991bafa1
HyundongdongYun <my4719cuswo12@gmail.com>
2024-11-27 03:56:16 +0900

    record by UserAndDate

--- src/main/java/com/opensoftware/babsanglab/dto/response/RecordResponseDto.java
+++ src/main/java/com/opensoftware/babsanglab/dto/response/RecordResponseDto.java
@@ -1,11 +1,13 @@
 package com.opensoftware.babsanglab.dto.response;
 
+import com.opensoftware.babsanglab.domain.enums.Mealtime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Builder
 @AllArgsConstructor
 public class RecordResponseDto {
+    Mealtime mealtime;
     Double calories;
     Double fat;
     Double protein;
--- src/main/java/com/opensoftware/babsanglab/repository/RecordRepository.java
+++ src/main/java/com/opensoftware/babsanglab/repository/RecordRepository.java
@@ -2,12 +2,18 @@ package com.opensoftware.babsanglab.repository;
 
 import com.opensoftware.babsanglab.domain.Record;
 import com.opensoftware.babsanglab.domain.User;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record,Long> {
     List<Record> findByUser(User user);
+    @Query("SELECT r FROM Record r WHERE r.user = :user AND r.date = :date")
+    List<Record> findByUserAndDate(@Param("user") User user, @Param("date") LocalDate date);
 }
