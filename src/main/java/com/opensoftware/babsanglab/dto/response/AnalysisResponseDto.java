4bdc9242e6d162b715389a1582cae3f62f005370
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-01 02:13:31 +0900

    feat : modify Entity

--- src/main/java/com/opensoftware/babsanglab/dto/response/AnalysisResponseDto.java
+++ src/main/java/com/opensoftware/babsanglab/dto/response/AnalysisResponseDto.java
@@ -14,4 +14,5 @@ public class AnalysisResponseDto {
     Double protein;
     Double carbs;
     Allergy allergy;
+    String medical_issue;
 }
--- src/main/java/com/opensoftware/babsanglab/dto/response/NotifyResponseDto.java
+++ src/main/java/com/opensoftware/babsanglab/dto/response/NotifyResponseDto.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
+
 @AllArgsConstructor
 @Getter
+@Builder
 public class NotifyResponseDto {
     String message;
 }
