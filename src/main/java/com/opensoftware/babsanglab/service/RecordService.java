d0fcd3eaeeb28545a4be6e2f29ee1b03ddc882de
HyundongdongYun <my4719cuswo12@gmail.com>
2024-11-30 17:18:26 +0900

    feat : ApiException

--- src/main/java/com/opensoftware/babsanglab/service/RecordService.java
+++ src/main/java/com/opensoftware/babsanglab/service/RecordService.java
@@ -6,6 +6,8 @@ import com.opensoftware.babsanglab.domain.User;
 import com.opensoftware.babsanglab.dto.request.RecordSearchDto;
 import com.opensoftware.babsanglab.dto.response.RecordResponseDto;
 import com.opensoftware.babsanglab.dto.response.ResponseDto;
+import com.opensoftware.babsanglab.exception.ApiException;
+import com.opensoftware.babsanglab.exception.ErrorDefine;
 import com.opensoftware.babsanglab.repository.RecordRepository;
 import com.opensoftware.babsanglab.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +22,10 @@ public class RecordService {
     private final RecordRepository recordRepository;
     private final UserRepository userRepository;
 
-    public List<RecordResponseDto> recordSearch(RecordSearchDto recordSearchDto){
-        User user = userRepository.findByName(recordSearchDto.getName());
+        public List<RecordResponseDto> recordSearch(RecordSearchDto recordSearchDto){
+        User user = userRepository.findByName(recordSearchDto.getName())
+                .orElseThrow(() -> new ApiException(ErrorDefine.USER_NOT_FOUND));
+
         List<Record> records = recordRepository.findByUser(user);
 
 //        List<RecordResponseDto> recordResponseDtos = records.stream()
@@ -36,16 +40,14 @@ public class RecordService {
 
         return records.stream()
                 .map(record -> RecordResponseDto.builder()
-                        .fat(record.getFat())
-                        .calories(record.getCalories())
-                        .protein(record.getProtein())
-                        .carbs(record.getCarbs())
                         .build()
                 ).toList();
     }
 
     public List<RecordResponseDto> recordDay(String userName, LocalDate date) {
-        User user = userRepository.findByName(userName);
+        User user = userRepository.findByName(userName)
+                .orElseThrow(() -> new ApiException(ErrorDefine.USER_NOT_FOUND));
+
         List<Record> records = recordRepository.findByUserAndDate(user, date);
 
         return records.stream()
