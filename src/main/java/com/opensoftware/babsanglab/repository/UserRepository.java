18a22a25cdf884c043011480ad99008eb65d9012
HyundongdongYun <my4719cuswo12@gmail.com>
2024-11-30 17:19:30 +0900

    feat : change type to Optional<User>

--- src/main/java/com/opensoftware/babsanglab/repository/UserRepository.java
+++ src/main/java/com/opensoftware/babsanglab/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
-    User findByName(String name);
+    Optional<User> findByName(String name);
 }
