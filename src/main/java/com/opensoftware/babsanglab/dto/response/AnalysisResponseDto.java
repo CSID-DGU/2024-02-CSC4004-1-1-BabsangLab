094dbc5d3925ec94af654031a9e31988ef6682c7
HyundongdongYun <my4719cuswo12@gmail.com>
2024-11-30 22:22:47 +0900

    feat : plus Getter Annotation

--- src/main/java/com/opensoftware/babsanglab/dto/response/AnalysisResponseDto.java
+++ src/main/java/com/opensoftware/babsanglab/dto/response/AnalysisResponseDto.java
@@ -3,7 +3,9 @@ package com.opensoftware.babsanglab.dto.response;
 import com.opensoftware.babsanglab.domain.enums.Allergy;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class AnalysisResponseDto {
