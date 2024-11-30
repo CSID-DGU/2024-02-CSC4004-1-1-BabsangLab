da665c18ffacbb0070ae16b0e88e9142567e31c4
HyundongdongYun <my4719cuswo12@gmail.com>
2024-11-30 22:23:54 +0900

    feat : add Getter Annotation

--- src/main/java/com/opensoftware/babsanglab/dto/response/RecordResponseDto.java
+++ src/main/java/com/opensoftware/babsanglab/dto/response/RecordResponseDto.java
@@ -3,7 +3,9 @@ package com.opensoftware.babsanglab.dto.response;
 import com.opensoftware.babsanglab.domain.enums.Mealtime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class RecordResponseDto {
